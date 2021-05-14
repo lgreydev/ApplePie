f0398772f8b87bc1f51d528345fc986621bf4793
Sergey Lukaschuk <s.lukaschuk@yahoo.com>
2021-05-14 19:09:14 +0300

    Add turn off/on buttons

--- ApplePie/Game.swift
+++ ApplePie/Game.swift
@@ -10,15 +10,19 @@ import Foundation
 
 class Game  {
     
+    // MARK: - Properties
     private let listOfWords = ListOfWords()
     private(set) lazy var word: String = listOfWords.city.randomElement()!
     private(set) var incorrectMovesRemaining: Int = 7
     private var guessedLetters: [Character] = []
     private(set) var totalWins = 0
     private(set) var totalLosses = 0
+    var onButton = false
     
+    
+    // MARK: - Letter Matching
+    // checking for matches of letters in a word
     var guessedWord: String {
-        print(word)
         var wordLabel = ""
         for letter in word.lowercased() {
             guessedLetters.contains(letter) ? wordLabel.append(letter) : wordLabel.append("_")
@@ -26,14 +30,38 @@ class Game  {
         return wordLabel.uppercased()
     }
     
+    
+    // MARK: - Count Apples
     func playerGuessed(letter: Character) {
         let lowerWord = word.lowercased()
         let loverLetter = letter.lowercased()
         if !lowerWord.contains(loverLetter) {
-            guard incorrectMovesRemaining > 0 else { fatalError("Game Over!") }
             incorrectMovesRemaining -= 1
         } else {
             guessedLetters.append(Character(loverLetter))
         }
+    }
+    
+    
+    // MARK: - State Game
+    func state() {
+        print(word, guessedWord)
+        let newWord = word.filter { !$0.isWhitespace }.lowercased()
+        if newWord == guessedWord.lowercased() {
+            totalWins += 1
+            newRound()
+        } else if incorrectMovesRemaining < 1 {
+            totalLosses += 1
+            newRound()
+        }
+    }
+    
+    
+    // MARK: - New Round
+    func newRound() {
+        word = listOfWords.city.randomElement()!
+        guessedLetters = [Character]()
+        incorrectMovesRemaining = 7
+        onButton = true
     }
 }
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -9,34 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    // MARK: - Game init
+    let game = Game()
+    
+    
     // MARK: - IB Outlet
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var letterButtons: [UIButton]!
     
-    
-    // MARK: - Game Setting
-    let game = Game()
-    
-    
+  
     // MARK: - Update UI
     func updateUI() {
+        game.state()
         let imageName = "Tree \(game.incorrectMovesRemaining)"
         treeImageView.image = UIImage(named: imageName)
         scoreLabel.text = "Win: \(game.totalWins)       Losses: \(game.totalLosses)"
         updateCorrectWordLabel()
+        buttonOn()
+        
     }
 
     func updateCorrectWordLabel() {
         var displayWord = [String]()
         for latter in game.guessedWord {
-                displayWord.append(String(latter))
+            displayWord.append(String(latter))
         }
         correctWordLabel.text = displayWord.joined(separator: " ")
     }
     
-
+    
+    // MARK: - Turn on Button
+    func buttonOn() {
+        for button in letterButtons {
+            if game.onButton {
+                button.isEnabled = true
+            }
+        }
+        game.onButton = false
+    }
+    
+    
     // MARK: - Action
     @IBAction func pressedLetterButton(_ sender: UIButton) {
         sender.isEnabled = false
